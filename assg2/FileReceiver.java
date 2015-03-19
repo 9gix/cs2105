ed6dbf4f2770e68b167b8552cb1435ee679b7e48
Eugene <yeo.eugene.oey@gmail.com>
2015-03-19 23:23:03 +0800

    Stop And Wait Reliable Data Transfer

---
+++ assg2/FileReceiver.java
@@ -0,0 +1,202 @@
+// Author: Eugene
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.zip.CRC32;
+
+class FileReceiver {
+
+    public DatagramSocket socket;
+    public DatagramPacket packet;
+    
+    byte[] packet_buffer;
+    public static final int PACKET_BUFFER_SIZE = 1000;
+    
+    byte[] packet_data;
+    
+    
+    public static final short SYN_MASK = 0b00000001;
+    public static final short ACK_MASK = 0b00000010;
+    public static final short FIN_MASK = 0b00000100;
+    
+
+    // Packet Data Structure 
+    private short data_length;
+    private int data_checksum;
+    private int seq_no;
+    private int ack_no;
+    private short flags;
+    private byte[] data;
+    // -------------------------
+    
+    // Receiver Data Structure
+    private File file;
+    private int receive_base; // Current Window Starting Sequence
+    
+    int current_offset = 0;
+    // -------------------------
+    private boolean is_corrupted;
+    private String filename;
+    private RandomAccessFile file_access;
+    
+    public static class Packet {
+        public static final int SEQ_BYTE_OFFSET = 0; // integer
+        public static final int SEQ_BYTE_LENGTH = 4;
+        public static final int ACK_BYTE_OFFSET = 4; // integer
+        public static final int ACK_BYTE_LENGTH = 4;
+        public static final int CHECKSUM_BYTE_OFFSET = 8; // integer
+        public static final int CHECKSUM_BYTE_LENGTH= 4;
+        public static final int FLAG_BYTE_OFFSET = 12; // short
+        public static final int FLAG_BYTE_LENGTH = 2;
+        public static final int DATA_BYTE_OFFSET = 14; // byte[]
+        public static final int DATA_BYTE_LENGTH = 986;
+        
+        public static final int HEADER_LENGTH = 14;
+    }
+    
+    
+
+    public static void main(String[] args) throws IOException {
+
+        // check if the number of command line argument is 1
+        if (args.length != 1) {
+            System.out.println("Usage: java FileReceiver port");
+            System.exit(1);
+        }
+
+        int port = Integer.parseInt(args[0]);
+        FileReceiver server = new FileReceiver(port);
+        server.serve_until_end_of_file();
+    }
+
+
+    public FileReceiver(int localPort) throws SocketException {
+        socket = new DatagramSocket(localPort);
+        packet_buffer = new byte[PACKET_BUFFER_SIZE];
+        packet = new DatagramPacket(packet_buffer, packet_buffer.length);
+    }
+
+    public void serve_until_end_of_file() throws IOException{
+        
+        receivePacket();
+        prepareFile();
+        
+        while (!isEndOfFile()) {
+            receivePacket();
+            if (!isEndOfFile()){
+                updateFile();
+            }
+        }
+    }
+    
+    private void receivePacket() throws IOException{
+        this.is_corrupted = true;
+        while (this.is_corrupted){
+            socket.receive(packet);
+            processPacket();
+            System.out.println(this.data_length);
+            System.out.println("Received Packet Seq: " + this.seq_no);
+        }
+        acknowledgePacket();
+    }
+    
+
+    private void acknowledgePacket() throws IOException {
+        this.ack_no = this.seq_no;
+        sendAck();
+    }
+
+
+    /***
+     * 
+     * @param packet_buffer
+     * @return true if data packet is correct
+     */
+    public static boolean verifyChecksum(ByteBuffer packetBuffer) {
+        int sent_checksum = packetBuffer.getInt(Packet.CHECKSUM_BYTE_OFFSET);
+        System.out.println(sent_checksum);
+        byte[] b = packetBuffer.array();
+        Arrays.fill(b, Packet.CHECKSUM_BYTE_OFFSET, Packet.CHECKSUM_BYTE_OFFSET + Packet.CHECKSUM_BYTE_LENGTH, (byte) 0);
+        int received_checksum = calculateChecksum(b);
+        return sent_checksum == received_checksum;
+    }
+    
+    public static int calculateChecksum(byte[] data){
+        CRC32 crc = new CRC32();
+        crc.update(data);
+        return (int) crc.getValue();
+    }
+
+    private void sendAck() throws IOException {
+
+        byte[] ack_buff = new byte[14];
+        DatagramPacket ack_packet = new DatagramPacket(
+                ack_buff, ack_buff.length, packet.getSocketAddress());
+        
+        ByteBuffer buffer = ByteBuffer.wrap(ack_buff);
+        buffer.putInt(0); // Sequence No
+        buffer.putInt(ack_no); // Ack No
+        buffer.putInt(0); // Checksum
+        buffer.putShort(ACK_MASK); // Flag
+        ack_packet.setLength(14);
+        
+        int checksum = calculateChecksum(buffer.array());
+        buffer.putInt(Packet.CHECKSUM_BYTE_OFFSET, checksum);
+        socket.send(ack_packet);
+        System.out.println("Sending Ack: " + this.ack_no);
+    }
+    
+    
+
+    private void updateFile() throws IOException {
+//        file = new File(filename);
+        
+//            current_offset = this.seq_no * Packet.DATA_BYTE_LENGTH;
+//            file_access.seek(current_offset);
+            file_access.write(this.data);
+        
+    }
+
+    private void prepareFile() throws IOException {
+        filename = new String(data).trim();
+        file = new File(filename);
+        if (file.exists()){
+            file.delete();
+        }
+        file.createNewFile();
+        file_access = new RandomAccessFile(file, "rw");
+    }
+    
+    private void processPacket() {
+        packet_data = packet.getData();
+        ByteBuffer packetBuffer = ByteBuffer.wrap(packet_data, 0, packet.getLength());
+        seq_no = packetBuffer.getInt();
+        ack_no = packetBuffer.getInt();
+        data_checksum = packetBuffer.getInt();
+        flags = packetBuffer.getShort();
+        data = new byte[packetBuffer.remaining()];
+        System.out.println(data.length);
+        packetBuffer.get(data);
+        is_corrupted = !verifyChecksum(packetBuffer);
+    }
+
+    private boolean isEndOfFile() {
+        return (flags & FIN_MASK) == FIN_MASK;
+    }
+    
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
---
+++ assg2/FileSender.java
@@ -0,0 +1,124 @@
+// Author: Eugene
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
+import java.util.zip.CRC32;
+
+class FileSender {
+    
+    
+    
+    public DatagramSocket socket; 
+    public DatagramPacket packet;
+    
+    byte[] packet_buffer;
+    private int seq_no;
+    
+    
+    public static void main(String[] args) throws IOException, InterruptedException {
+        
+        // check if the number of command line argument is 4
+        if (args.length != 4) {
+            System.out.println("Usage: java FileSender <path/filename> "
+                                   + "<rcvHostName> <rcvPort> <rcvFileName>");
+            System.exit(1);
+        }
+        
+        String source_filename = args[0];
+        String hostname = args[1];
+        int port = Integer.parseInt(args[2]);
+        SocketAddress socket_address = new InetSocketAddress(hostname, port);
+        String destination_filename = args[3];
+        new FileSender(source_filename, socket_address, destination_filename);
+    }
+    
+    public FileSender(String sourceFilename, SocketAddress socket_address, String destinationFilename) throws IOException, InterruptedException {
+        
+        packet_buffer = new byte[FileReceiver.PACKET_BUFFER_SIZE];
+        socket = new DatagramSocket();
+        socket.setSoTimeout(200);
+        packet = new DatagramPacket(packet_buffer, packet_buffer.length, socket_address);
+        
+        sendFile(sourceFilename, destinationFilename);
+    }
+    
+    private void sendFile(String sourceFilename, String destinationFilename) throws IOException {
+        byte[] fileMetaData = new byte[FileReceiver.Packet.DATA_BYTE_LENGTH];
+        ByteBuffer buffer = ByteBuffer.wrap(fileMetaData);
+        buffer.put(destinationFilename.getBytes());
+        send(buffer.array(), false);
+        
+        try (InputStream input_stream = new BufferedInputStream(new FileInputStream(sourceFilename))){
+            int dataLength;
+            byte[] data_reader_buffer = new byte[FileReceiver.Packet.DATA_BYTE_LENGTH];
+            while((dataLength = input_stream.read(data_reader_buffer)) > 0){
+                seq_no++;
+                ByteArrayOutputStream out_stream = new ByteArrayOutputStream();
+                out_stream.write(data_reader_buffer, 0, dataLength);
+                byte[] dataBuffer = out_stream.toByteArray();
+                send(dataBuffer, false);
+            }
+            
+        }
+        send(new byte[0], true);
+    }
+
+    private void send(byte[] data, boolean isEOF) throws IOException {
+        buildPacket(data, isEOF);
+        boolean waitForAck = true;
+        byte[] ackBuffArr = new byte[14];
+        DatagramPacket ackPacket = new DatagramPacket(ackBuffArr, ackBuffArr.length);
+        while (waitForAck){
+            if (isEOF){
+                System.out.println("Sending Packet FIN");
+            } else {
+                System.out.println("Sending Packet Seq :" + seq_no);
+            }
+            socket.send(packet);
+            try {
+                socket.receive(ackPacket);
+                ByteBuffer ackBuffer = ByteBuffer.wrap(ackBuffArr);
+                int ack_no = getAckNo(ackPacket);
+                System.out.println("Received Ack:" + ack_no);
+                
+                waitForAck = !FileReceiver.verifyChecksum(ackBuffer);
+            } catch (SocketTimeoutException e){
+                waitForAck = true;
+            }
+        }
+    }
+
+    private int getAckNo(DatagramPacket ackPacket) {
+        byte[] dataBuffArr = ackPacket.getData();
+        ByteBuffer ackBuffer = ByteBuffer.wrap(dataBuffArr);
+        return ackBuffer.getInt(FileReceiver.Packet.ACK_BYTE_OFFSET);
+    }
+
+    private void buildPacket(byte[] dataBuffer, boolean isEOF) {
+        ByteBuffer buffer = ByteBuffer.wrap(packet_buffer);
+        buffer.putInt(seq_no);
+        buffer.putInt(0);
+        buffer.putInt(0);
+        short flags = FileReceiver.SYN_MASK;
+        if (isEOF){
+            flags |= FileReceiver.FIN_MASK;
+        }
+        buffer.putShort(flags);
+        buffer.put(dataBuffer);
+        int checksum = FileReceiver.calculateChecksum(buffer.array());
+        buffer.putInt(FileReceiver.Packet.CHECKSUM_BYTE_OFFSET, checksum);
+        packet.setLength(FileReceiver.Packet.HEADER_LENGTH + dataBuffer.length);
+    }
+
+}
